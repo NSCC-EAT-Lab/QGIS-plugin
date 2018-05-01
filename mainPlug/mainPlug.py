37894af1fb8a564ee6a6744b1fa1cd4bc183d756
Zachary Higgs <compgeek223@gmail.com>
2018-05-01 10:55:00 -0300

    Remove Autogenerated Licenses

--- mainPlug/mainPlug.py
+++ mainPlug/mainPlug.py
@@ -10,15 +10,6 @@
         copyright            : (C) 2018 by EAT Labs
         email                : foo@bar.com
  ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
 import os.path
 import re
