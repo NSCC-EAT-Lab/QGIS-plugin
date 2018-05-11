156f3d3b46113407aec2475a6c1d451320401437
Zachary Higgs <compgeek223@gmail.com>
2018-05-11 10:50:31 -0300

    Adding in the bindings to Plug the new module in

--- mainPlug/Interpolate.py
+++ mainPlug/Interpolate.py
@@ -2,6 +2,7 @@
 from processing.core.Processing import Processing
 from processing.tools import *
 from UseCommunication import Communicate
+from qgis.core import QgsVectorLayer
 
 
 # from qgis.core import QgsPro
@@ -12,7 +13,8 @@ class interp():
         self.iface = iface
         self.com = Communicate(self.iface)
         self.pLayer = pointLayer
-
+        self.PredictionLayer = QgsVectorLayer()
+        self.VarianceLayer = QgsVectorLayer()
 
     def run_Output(self):
         alg = 'saga:simplekriging'
@@ -27,12 +29,22 @@ class interp():
 
         coords = "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax)
 
+
+
         params = {"POINTS": self.pLayer, "FIELD": self.pLayer.name(), "TQUALITY": 0, "LOG": False,
                   "BLOCK": False,
                   "DBLOCK": 1, "VAR_MAXDIST": -1, "VAR_NCLASSES": 100, "VAR_NSKIP": 1, "VAR_MODEL": "a+b*x",
                   "OUTPUT_EXTENT": coords, "TARGET_USER_SIZE": 0.000001, "TARGET_USER_FITS": 0,
                   "SEARCH_RANGE": 0, "SEARCH_RADIUS": 1000, "SEARCH_POINTS_ALL": 0, "SEARCH_POINTS_MIN": 4,
-                  "SEARCH_POINTS_MAX": 20, "SEARCH_DIRECTION": 0}
+                  "SEARCH_POINTS_MAX": 20, "SEARCH_DIRECTION": 0, "PREDICTION": self.PredictionLayer, "VARIANCE": self.VarianceLayer}
 
+        # params = {"POINTS": iface.activeLayer(), "FIELD": iface.activeLayer().name(), "TQUALITY": 0, "LOG": False, "BLOCK": False,
+        #            "DBLOCK": 1, "TARGET_USER_SIZE": 0.000001}
 
         processing.runalg(alg, params)
+
+    def get_PredictionLayer(self):
+        return self.PredictionLayer
+
+    def get_VarianceLayer(self):
+        return self.VarianceLayer
--- mainPlug/krig_dialog.py
+++ mainPlug/krig_dialog.py
@@ -17,20 +17,27 @@ import os
 from PyQt4 import QtGui, uic
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'mainPlug_dialog_base.ui'))
+    os.path.dirname(__file__), 'KrigDialog.ui'))
 
 
-class mainPlugDialog(QtGui.QDialog, FORM_CLASS):
+class KrigDialog(QtGui.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """
         This is an Error Screen, Create the error screen
         :param parent:
         """
-        super(mainPlugDialog, self).__init__(parent)
+        super(KrigDialog, self).__init__(parent)
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots - see
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
+        self.buttonBox.clicked.connect(self.retval)
+        self.processall = False
 
+    def retval(self):
+        self.processall = self.Processall.isChecked()
+
+    def retProcessallState(self):
+        return self.processall
